940eee417fa1cf73b6d6abd8fc32fed7921433d4
shishao-home <shishaoqi0928@gmail.com>
2021-07-16 00:17:48 +0800

    更新 c_pointer.c

--- 【C vs Go】7：指针/c_pointer.c
+++ 【C vs Go】7：指针/c_pointer.c
@@ -35,30 +35,10 @@ int main() {
 
     printf("p person info: \n");
     printf("p.age: %d\n", p->age);
+    printf("p.age: %d\n", *((int*)p));
+
     printf("p.phone: %s\n", p->phone);
+    printf("p.phone: %c\n", *((char*)(p+8+8)));
+
     printf("p.name: %s\n", p->name);
-
-    //一次分配所需内存
-    void *pt;
-    pt = (void*)malloc(sizeof(personType) + 8 + 12 + 11);
-
-    personType *p2;
-    char *phone;
-    char *name;
-    int age;
-
-    *((int *)pt) = 99;
-    age = *((int *)pt);
-    printf("age = %d \n", age);
-
-    *((char**)pt + 8) = "18259179840\0";
-    // phone = (char*)((char*)pt + 8);
-    // phone = "18259179840";
-    // printf("phone = %s \n", phone);
-    printf("(char*)((char*)pt + 8) = %s \n", (char*)((char*)pt + 8));
-
-    name = (char*)((char*)pt + 8 + 12);
-    name = "shishao-12\0";
-    printf("name = %s \n", name);
-    printf("(char*)((char*)pt + 8 + 12) = %s \n", ((char*)pt + 8 + 12));
  }
